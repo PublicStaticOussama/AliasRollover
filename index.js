87c05ed6d44cfe9a3dc63343dc07a30a5b0d9fb4
PublicStaticOussama <130459339+PublicStaticOussama@users.noreply.github.com>
2024-11-10 22:50:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,38 +26,3 @@ const rolloverIndices = await fetchRolloverIndices(
 
 if (rolloverIndices.length) await rollover(rolloverIndices);
 
-// const res = await client.indices.getMapping({
-//   index: [].map(({ index }) => index),
-// });
-
-// const data = fs.readFileSync("./blunder.json", "utf8");
-// const blunders = JSON.parse(data);
-// console.log(blunders[0]);
-// console.log("=============================================;");
-// console.log("=============================================;");
-
-// const indices = blunders.map((blunder) => blunder.index);
-// console.log(indices);
-// console.log(indices.length);
-
-// console.log("=============================================;");
-// console.log("=============================================;");
-
-// const res = await client.search({
-//   index: indices,
-// });
-
-// const noneEmptyIndices = res.body.hits.hits.map((hit) => hit._index);
-// console.log(noneEmptyIndices);
-// const noneEmptyIndexSet = new Set(noneEmptyIndices);
-// console.log(noneEmptyIndexSet);
-
-// const i = indices.indexOf(noneEmptyIndices[0]);
-// console.log("indexOf:", i);
-// console.log(indices[i]);
-
-// const res2 = await client.indices.delete({
-//   index: indices,
-// });
-
-// console.log(res2.statusCode);
